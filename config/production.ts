6450542efbe9a477d08bdf8c48fcadabce24120f
idarbuashvili <iraklid94@gmail.com>
2023-06-11 22:30:10 +0400

    build: :rocket: Add deployment to app engine mdesign-backend service
    Update app.yaml file for deployment on app engine mdesign-backend service; Updated gcloudignore file to exclude all except whats necessary; Updated production config with database info; Updated some of the packages

--- config/production.ts
+++ config/production.ts
@@ -1,6 +1,7 @@
 export default {
   port: 8080,
   database: {
-    connection: '<CONNECTION_STRING_HERE>',
+    connection:
+      'mongodb+srv://mdesigndb:LxQjD9USz11rrq8B@cluster0.0gtzqr6.mongodb.net/mdesigndb?retryWrites=true&w=majority',
   },
 }
