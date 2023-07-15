add72aacb3ff748922a7b7a76791d475c5171505
idarbuashvili <iraklid94@gmail.com>
2023-07-15 14:56:26 +0400

    Update AI configs, reduced inference steps for stable diffusion to 25, as giving more then that not gives any significant results. Update some of the packages

--- config/default.ts
+++ config/default.ts
@@ -41,17 +41,18 @@ export default {
   },
   predictionProvider: {
     stableDiffusion: {
-      URL: 'http://localhost:7080/predictions/stable_diffusion-0.0.39',
+      URL: 'http://localhost:7080/predictions/stable_diffusion-0.0.40',
       prompt:
         'Transform the given space into a fully furnished %s %s room with as much furniture as possible while maintaining a functional and visually appealing layout, taking into account walls and layout.',
-      inference_steps: 100,
+      inference_steps: 25,
+      // inference_steps: 100,
       inference_strength: 0.65,
       inference_guidance_scale: 25,
       num_return_images: 3,
       generator_seed: -11,
     },
     detrResNet: {
-      URL: 'http://localhost:7080/predictions/detr_resnet-0.0.39',
+      URL: 'http://localhost:7080/predictions/detr_resnet-0.0.40',
     },
   },
   paginationLimit: 10,
