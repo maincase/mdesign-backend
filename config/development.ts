3c7cbef182151fa106fceb6eac0d1c6fb0aae80c
idarbuashvili <iraklid94@gmail.com>
2023-08-17 16:41:50 +0300

    Update packages used by the app, update config files with correct AI model urls

--- config/development.ts
+++ config/development.ts
@@ -1,10 +1,10 @@
 export default {
-  // predictionProvider: {
-  //   stableDiffusion: {
-  //     URL: 'https://us-central1-aiplatform.googleapis.com/v1/projects/modern-design-ai/locations/us-central1/endpoints/6296643607528996864:predict',
-  //   },
-  //   detrResNet: {
-  //     URL: 'https://us-central1-aiplatform.googleapis.com/v1/projects/modern-design-ai/locations/us-central1/endpoints/7022286097489068032:predict',
-  //   },
-  // },
+  predictionProvider: {
+    stableDiffusion: {
+      URL: 'http://localhost:7080/predictions/stable_diffusion-0.0.48',
+    },
+    detrResNet: {
+      URL: 'http://localhost:7080/predictions/detr_resnet-0.0.48',
+    },
+  },
 }
--- config/production.ts
+++ config/production.ts
@@ -1,1 +1,10 @@
-export default {}
+export default {
+  predictionProvider: {
+    stableDiffusion: {
+      URL: 'https://us-central1-aiplatform.googleapis.com/v1/projects/modern-design-ai/locations/us-central1/endpoints/6296643607528996864:predict',
+    },
+    detrResNet: {
+      URL: 'https://us-central1-aiplatform.googleapis.com/v1/projects/modern-design-ai/locations/us-central1/endpoints/7022286097489068032:predict',
+    },
+  },
+}
