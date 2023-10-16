a6e7ddccd43ead844467e01e7fd45fbd1a4ccc37
idarbuashvili <iraklid94@gmail.com>
2023-10-16 05:21:02 +0400

    Fix correct prediction URL for resnet50 model

--- config/production.ts
+++ config/production.ts
@@ -1,7 +1,7 @@
 export default {
   predictionProvider: {
     detrResNet: {
-      URL: 'https://us-central1-aiplatform.googleapis.com/v1/projects/modern-design-ai/locations/us-central1/endpoints/1420764736156336128:predict',
+      URL: 'https://us-central1-aiplatform.googleapis.com/v1/projects/modern-design-ai/locations/us-central1/endpoints/5739716778804641792:predict',
     },
   },
 }
