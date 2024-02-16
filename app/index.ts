2e25cd21986413c07e223f1b7c610d06882dbbe6
idarbuashvili <iraklid94@gmail.com>
2024-02-16 16:21:14 +0400

    Disable cors in production

--- app/index.ts
+++ app/index.ts
@@ -24,23 +24,23 @@ const multerMid = Util.promisify(
   }).single('file')
 )
 
-// if (process.env.NODE_ENV !== 'production') {
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Credentials', 'true')
-  res.header('Access-Control-Allow-Origin', req.headers.origin)
-  res.header('Access-Control-Allow-Methods', 'OPTIONS,GET,PUT,POST,DELETE')
-  res.header(
-    'Access-Control-Allow-Headers',
-    'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, authorization, TimeZone'
-  )
+if (process.env.NODE_ENV !== 'production') {
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Credentials', 'true')
+    res.header('Access-Control-Allow-Origin', req.headers.origin)
+    res.header('Access-Control-Allow-Methods', 'OPTIONS,GET,PUT,POST,DELETE')
+    res.header(
+      'Access-Control-Allow-Headers',
+      'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, authorization, TimeZone'
+    )
 
-  if (req.method === 'OPTIONS') {
-    res.sendStatus(200)
-  } else {
-    next()
-  }
-})
-// }
+    if (req.method === 'OPTIONS') {
+      res.sendStatus(200)
+    } else {
+      next()
+    }
+  })
+}
 
 app.set('superSecret', config.secret)
 app.use(bodyParser.json())
